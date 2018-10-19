d40965620eaaf68a3126383bc381543a779a6a17
jivinjules <juliehodges05@gmail.com>
2018-10-19 17:20:03 -0500

    functional

--- public/app.js
+++ public/app.js
@@ -1,90 +1,69 @@
-$(document).ready(function () {
-  /////SCRAPE
-  $("#scrape-btn").on("click", function (event) {
-    event.preventDefault()
+//SCRAPE
+$(".save").on("click", function() {
     $.ajax({
-      method: "GET",
-      url: "/scrape"
+        method: "GET",
+        url: "/scrape",
+    }).done(function(data) {
+        console.log(data)
+        window.location = "/"
     })
-      .then(function (data) {
-        console.log(data);
-        location.reload()
+});
 
-      })
-  })
-
-  ////GET AN ARTICLE
-  $("#article-btn").on("click", function () {
-
+//SAVE ARTICLE
+$(".save").on("click", function() {
     var thisId = $(this).attr("data-id");
     $.ajax({
-      method: "GET",
-      url: "/articles/" + thisID
+        method: "POST",
+        url: "/articles/save/" + thisId
+    }).done(function(data) {
+        window.location = "/"
     })
-      .then(function (data) {
-        location.reload()
-      })
-  })
+});
 
-  /////SAVE AN ARTICLE
-  $(document).on("click", "#save-btn", function (event) {
-    console.log("click")
-    var savedArticle = $(this).attr("data-articleid");
-    // savedArticle.saved = true;
-
+//DELETE ARTICLE
+$(".delete").on("click", function() {
+    var thisId = $(this).attr("data-id");
     $.ajax({
-      method: "GET",
-      url: "/articles/" + savedArticle
-    }).then(function () {
-      var hbsObject = {
-            articles: dbArticle
-        };
+        method: "POST",
+        url: "/articles/delete/" + thisId
+    }).done(function(data) {
+        window.location = "/saved"
+    })
+});
+
+//SAVE NOTE
+$(".saveNote").on("click", function() {
+    var thisId = $(this).attr("data-id");
+    if (!$("#noteText" + thisId).val()) {
+        alert("please enter a note to save")
+    }else {
+      $.ajax({
+            method: "POST",
+            url: "/notes/save/" + thisId,
+            data: {
+              text: $("#noteText" + thisId).val()
+            }
+          }).done(function(data) {
+              // Log the response
+              console.log(data);
+              // Empty the notes section
+              $("#noteText" + thisId).val("");
+              $(".modalNote").modal("hide");
+              window.location = "/saved"
+          });
     }
-    )
-  })
+});
 
-  $('body').on('click', function () {
-    console.log('body was clicked')
-  })
-
-  /////CLEAR OUT ALL ARTICLES
-  $(document).on("click", "#delete-btn", function () {
-
+//DELTE NOTE
+$(".deleteNote").on("click", function() {
+    var noteId = $(this).attr("data-note-id");
+    var articleId = $(this).attr("data-article-id");
     $.ajax({
-      method: "GET",
-      url: "/clear"
-    }).then(function () {
-      console.log("Articles Cleared")
-      window.location.href = "/";
-    });
-  });
-
-
-  // Run a POST request to change the note, using what's entered in the inputs
-  $(document).on("click", "#comment", function () {
-    $("#add-note-modal").modal('toggle')
-    var thisId = $(this).attr("data-articleid");
-
-    $.ajax({
-      method: "POST",
-      url: "/saved/" + thisId,
-      data: {
-        // Value taken from title input
-        title: $("#comment-title").val(),
-        // Value taken from note textarea
-        body: $("#comment-body").val()
-      }
+        method: "DELETE",
+        url: "/notes/delete/" + noteId + "/" + articleId
+    }).done(function(data) {
+        console.log(data)
+        $(".modalNote").modal("hide");
+        window.location = "/saved"
     })
-      // With that done
-      .then(function (data) {
-        // Log the response
-        console.log(data);
-        // Empty the notes section
-        $("#notes").empty();
-      });
-
-    // Also, remove the values entered in the input and textarea for note entry
-    $("#comment-title").val("");
-    $("#comment-body").val("");
-  });
 });
--- server.js
+++ server.js
@@ -1,42 +1,246 @@
+// Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
 var logger = require("morgan");
+var mongoose = require("mongoose");
+var path = require("path");
 
-var PORT = process.env.PORT || 3030;
+//Models
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
 
+// Scraping tools
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+//Port
+var PORT = process.env.PORT || 3030
+
+// Initialize Express
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// parse application/json
-app.use(bodyParser.json());
-
-// Use morgan logger for logging requests
+// Use morgan and body parser with our app
 app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
-//connecting to MongoDB
-mongoose.connect('mongodb://localhost/news-scraper', { useNewUrlParser: true });
-
+// Make public a static dir
+app.use(express.static("public"));
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({
+    defaultLayout: "main",
+    partialsDir: path.join(__dirname, "/views/layouts/partials")
+}));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controller/news_controller.js");
+mongoose.connect("mongodb://localhost/news-scraper");
 
-// ////////////////////////// CHECK THIS
-app.use(routes);
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost: " + PORT);
+////////////////////////ROUTES TO MAIN PAGE
+app.get("/", function (req, res) {
+    Article.find({ "saved": false }, function (error, data) {
+        var hbsObject = {
+            article: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+app.get("/saved", function (req, res) {
+    Article.find({ "saved": true }).populate("notes").exec(function (error, articles) {
+        var hbsObject = {
+            article: articles
+        };
+        res.render("saved", hbsObject);
+    });
+});
+
+///////////////////////////ROUTES TO SCRAPE
+app.get("/scrape", function (req, res) {
+    ////get html
+    axios.get("https://www.nytimes.com/section/us", function (error, response, html) {
+        // Then, we load that into cheerio and save it to $ for a shorthand selector
+        var $ = cheerio.load(html);
+        $("div.story-body").each(function (i, element) {
+
+            var result = {};
+
+            // Add the text and href of every link, and save them as properties of the result object
+            result.title = $(element)
+                .children("h2.headline")
+                .text();
+            result.link = $(element)
+                .find("a")
+                .attr("href");
+            result.summary = $(element)
+                .find("p.summary")
+                .text();
+
+            var entry = new Article(result);
+
+            // save to database
+            entry.save(function (err, data) {
+                // Log any errors
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    console.log(data);
+                }
+            });
+
+        });
+        res.send("Scrape Complete");
+
+    });
+});
+
+
+//////////ROUTE: CLEAR UNSAVED
+app.get('/clear', function(req, res) {
+    db.Article.remove({ saved: false}, function(err, doc) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('removed');
+        }
+
+    });
+    res.redirect('/');
+});
+
+////////////////////Gets the JSON
+app.get("/articles", function (req, res) {
+    // Grab every doc in the Articles array
+    Article.find({}, function (error, data) {
+        // Log any errors
+        if (error) {
+            console.log(error);
+        }
+        // Or send the data to the browser as a json object
+        else {
+            res.json(data);
+        }
+    });
+});
+
+///////////////////////ROUTE FOR AN ARTICLE
+app.get("/articles/:id", function (req, res) {
+
+    Article.findOne({ "_id": req.params.id })
+        //Populate note
+        .populate("note")
+        /////////////////////?????ASK MATT ABOUT .then vs .exec????
+        .exec(function (error, data) {
+            // Log any errors
+            if (error) {
+                console.log(error);
+            }
+            else {
+                res.json(data);
+            }
+        });
+});
+
+
+///////////////////////ROUTES TO SAVE
+app.post("/articles/save/:id", function (req, res) {
+    // Use the article id to find and update its saved boolean
+    Article.findOneAndUpdate({ "_id": req.params.id }, { "saved": true })
+        // Execute the above query
+        .exec(function (err, data) {
+            // Log any errors
+            if (err) {
+                console.log(err);
+            }
+            else {
+
+                res.send(data);
+            }
+        });
+});
+
+//////////////////////////ROUTE TO DELETE
+app.post("/articles/delete/:id", function (req, res) {
+    //Anything not saved
+    Article.findOneAndUpdate({ "_id": req.params.id }, { "saved": false, "notes": [] })
+
+        .exec(function (err, data) {
+            // Log any errors
+            if (err) {
+                console.log(err);
+            }
+            else {
+                res.send(data);
+            }
+        });
+});
+
+
+////////////////////////ROUTE FOR COMMENT
+app.post("/notes/save/:id", function (req, res) {
+    // Create a new note and pass the req.body to the entry
+    var newNote = new Note({
+        body: req.body.text,
+        article: req.params.id
+    });
+    console.log(req.body)
+    // And save the new note the db
+    newNote.save(function (error, note) {
+
+        if (error) {
+            console.log(error);
+        }
+        else {
+            Article.findOneAndUpdate({ "_id": req.params.id }, { $push: { "notes": note } })
+                /////???EXEC VS THEN???
+                .exec(function (err) {
+
+                    if (err) {
+                        console.log(err);
+                        res.send(err);
+                    }
+                    else {
+                        res.send(note);
+                    }
+                });
+        }
+    });
+});
+
+/////////////////////////ROUTE TO DELTE A NOTE
+app.delete("/notes/delete/:note_id/:article_id", function (req, res) {
+    // Use the note id to find and delete it
+    Note.findOneAndRemove({ "_id": req.params.note_id }, function (err) {
+        // Log any errors
+        if (err) {
+            console.log(err);
+            res.send(err);
+        }
+        else {
+            Article.findOneAndUpdate({ "_id": req.params.article_id }, { $pull: { "notes": req.params.note_id } })
+                // Execute the above query
+                .exec(function (err) {
+                    // Log any errors
+                    if (err) {
+                        console.log(err);
+                        res.send(err);
+                    }
+                    else {
+                        // Or send the note to the browser
+                        res.send("Note Deleted");
+                    }
+                });
+        }
+    });
+});
+
+// Listen on port
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT);
 });
